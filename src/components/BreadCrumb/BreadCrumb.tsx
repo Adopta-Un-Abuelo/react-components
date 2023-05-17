f95ef1e780052ab63c368bbc66e9e27e0c185230
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2023-05-17 17:49:41 +0200

    feat: init storybook

---
+++ src/components/BreadCrumb/BreadCrumb.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, ComponentPropsWithoutRef } from 'react';
+import styled from 'styled-components';
+
+import Color from '../../constants/Color'; 
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+const Step = styled.div<{isSelected: boolean}>`
+    display: flex;
+    height: 8px;
+    width: 8px;
+    border-radius: 5px;
+    margin-right: 16px;
+    background-color: ${props => props.isSelected ? Color.background.primary : Color.text.low};
+`
+
+const BreadCrumb = (props: Props) =>{
+
+    const [ selectedStep, setSelectedStep ] = useState<number>(props.selectedStep ? props.selectedStep : 0);
+    const stepsArray = Array.from(Array(props.steps).keys());
+
+    useEffect(() =>{
+        if(props.selectedStep)
+            setSelectedStep(props.selectedStep);
+    }, [props.selectedStep]);
+
+    return(
+        <Container
+            {...props}
+        >
+            {stepsArray.map((item, index) =>(
+                <Step
+                    key={'crumb'+index}
+                    isSelected={selectedStep === index}
+                />
+            ))}
+        </Container>
+    )
+}
+export default BreadCrumb;
+export interface Props extends ComponentPropsWithoutRef<"div">{
+    selectedStep?: number
+    steps: number
+}
---
+++ src/stories/BreadCrumb.stories.tsx
@@ -0,0 +1,14 @@
+import BreadCrumb from '../components/BreadCrumb/BreadCrumb';
+
+export default {
+	title: 'Design System/BreadCrumb',
+	component: BreadCrumb,
+	tags: ['autodocs']
+};
+
+export const Default = {
+	args: {
+		selectedStep: 3,
+		steps: 8,
+	},
+};
