5d656f5bbfc3c0e7add33e4df423a1bf9020a626
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2023-05-19 10:42:44 +0200

    fix: feedback test

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -41,8 +41,7 @@ const FeedBack = ({type='success', isVisible=false, ...props}: Props) =>{
     },[isVisible]);
 
     return(show ?
-        <Container 
-            data-testid="feedback" 
+        <Container
             type={type} 
             {...props}
         >
--- src/stories/Feedback.stories.tsx
+++ src/stories/Feedback.stories.tsx
@@ -1,11 +1,19 @@
-import Feedback from '../components/Feedback/Feedback';
-import { within } from '@storybook/testing-library';
+import { useState } from 'react';
+import { userEvent, within } from '@storybook/testing-library';
 import { action } from '@storybook/addon-actions';
+
+import Feedback from '../components/Feedback/Feedback';
 
 export default {
 	title: 'Design System/Feedback',
 	component: Feedback,
 	tags: ['autodocs'],
+    args: {
+        isVisible: false,
+		type: 'success',
+        text: 'Test text',
+        onClose: action('onClose')
+    },
     argTypes: {
         isVisible: {
             type: {
@@ -42,15 +50,59 @@ export default {
     }
 };
 
-export const Default = {
-	args: {
-		isVisible: false,
-		type: 'success',
-        text: 'Test text',
-        onClose: action('onClose')
-	},
-	play: async ({canvasElement}: any) =>{
-		const canvas = within(canvasElement);
-		canvas.getByTestId('feedback');
-	}
-};
+export const Success = (args: any) =>{
+    const [ isVisible, setIsVisible ] = useState(false);
+    return(
+        <div>
+            <button
+                data-testid="button" 
+                onClick={() => setIsVisible(true)}
+            >
+                Show feedback modal
+            </button>
+            <Feedback
+                {...args}
+                isVisible={isVisible}
+                onClose={() => {
+                    setIsVisible(false);
+                    action('onClose');
+                }}
+            />
+        </div>
+    )
+}
+Success.play = async ({canvasElement}: any) =>{
+    const canvas = within(canvasElement);
+    const button = await canvas.getByTestId('button');
+    await userEvent.click(button);
+}
+
+export const Error = (args: any) =>{
+    const [ isVisible, setIsVisible ] = useState(false);
+    return(
+        <div>
+            <button
+                data-testid="button" 
+                onClick={() => setIsVisible(true)}
+            >
+                Show feedback modal
+            </button>
+            <Feedback
+                {...args}
+                isVisible={isVisible}
+                onClose={() => {
+                    setIsVisible(false);
+                    action('onClose');
+                }}
+            />
+        </div>
+    )
+}
+Error.args = {
+    type: 'error'
+}
+Error.play = async ({canvasElement}: any) =>{
+    const canvas = within(canvasElement);
+    const button = await canvas.getByTestId('button');
+    await userEvent.click(button);
+}
