9eb0941daa2411e8f77f0fadacb8f72e07b4cbe4
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2023-05-25 15:13:29 +0200

    fix: modal export

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export { default as Button } from './Button/Button';
 export { default as Input } from './Input/Input';
 export { default as Select } from './Select/Select';
 export { default as SearchBar } from './SearchBar/SearchBar';
+export { default as Modal } from './Modal/Modal';
