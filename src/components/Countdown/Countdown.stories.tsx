4bf6059b33d82c9616b284a735d7f89004ada54f
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2023-06-19 12:12:10 +0200

    feat: Countdown component

---
+++ src/components/Countdown/Countdown.stories.tsx
@@ -0,0 +1,25 @@
+import Countdown from './Countdown';
+import Color from '../../constants/Color';
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+export default {
+	title: 'Components/Countdown',
+	component: Countdown,
+	tags: ['autodocs'],
+    args: {
+        toDate: new Date('2024-01-01'),
+        color: Color.background.primaryLow,
+        textColor: Color.background.primary
+    }
+};
+
+export const Default = {
+	play: async ({canvasElement, step}: any) =>{
+		const canvas = within(canvasElement);
+		const container = await canvas.getByRole('container');
+        await step('render', async () =>{
+            expect(container).toBeInTheDocument();
+        })
+	}
+};
