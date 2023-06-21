251aad70d39f5c432f2e612fa6e9f9ea7d84ec50
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2023-06-21 12:05:10 +0200

    feat: text component updated

---
+++ src/components/Text/Button.tsx
@@ -0,0 +1,83 @@
+import { ComponentPropsWithoutRef } from 'react';
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+import Color from '../../constants/Color';
+
+const B1 = styled.p`
+	padding: 0px;
+	margin: 0px;
+	font-family: 'Poppins', 'sans-serif';
+	color: ${Color.text.full};
+	font-size: 18px;
+	${media.lessThan("medium")`
+		font-size: 15px;
+    `}
+`
+const B2 = styled.p`
+	padding: 0px;
+	margin: 0px;
+	font-family: 'Poppins', 'sans-serif';
+	color: ${Color.text.full};
+	font-size: 15px;
+	${media.lessThan("medium")`
+		font-size: 14px;
+    `}
+`
+const B3 = styled.p`
+	padding: 0px;
+	margin: 0px;
+	font-family: 'Poppins', 'sans-serif';
+	color: ${Color.text.full};
+	font-size: 13px;
+	${media.lessThan("medium")`
+		font-size: 13px;
+    `}
+`
+
+const Button = (props: ButtonProps) =>{
+
+	const { children, type, weight, style, ...rest } = props;
+
+	if(type === 'b1')
+		return(
+			<B1
+				style={{
+					fontWeight: weight === 'semibold' ? 600 : weight === 'medium' ? 500 : 400,
+					...style
+				}}
+				{...rest}
+			>
+				{children}
+			</B1>
+		)
+	else if(type === 'b2')
+		return(
+			<B2
+				style={{
+					fontWeight: weight === 'semibold' ? 600 : weight === 'medium' ? 500 : 400,
+					...style
+				}}
+				{...rest}
+			>
+				{children}
+			</B2>
+		)
+	else
+		return(
+			<B3
+				style={{
+					fontWeight: weight === 'semibold' ? 600 : weight === 'medium' ? 500 : 400,
+					...style
+				}}
+				{...rest}
+			>
+				{children}
+			</B3>
+		)
+}
+export default Button;
+export interface ButtonProps extends ComponentPropsWithoutRef<"p">{
+	type: 'b1' | 'b2' | 'b3',
+	weight?: 'semibold' | 'medium' | 'regular'
+}
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,22 +1,21 @@
-import React, { ComponentPropsWithoutRef } from 'react';
+import Header, { HeaderProps } from './Header';
+import Paragraph, { ParagraphProps } from './Paragraph';
+import Button, { ButtonProps } from './Button';
 
-import Header from './Header';
-import Paragraph from './Paragraph';
+const Text = (props: HeaderProps | ParagraphProps | ButtonProps) =>{
 
-const Text = (props: Props) =>{
-
-	return((props.type === 'h1' || props.type === 'h2' || props.type === 'h3' || props.type === 'h4' || props.type === 'h5' || props.type === 'h6') ?
+	return((props.type === 'd1' || props.type === 'h1' || props.type === 'h2' || props.type === 'h3' || props.type === 'h4' || props.type === 'h5' || props.type === 'h6') ?
         <Header role="header" {...props}>
             {props.children}
         </Header>
-    :
+    : (props.type === 'p' || props.type === 'p2' || props.type === 'c1' || props.type === 'c2' || props.type === 'o1' || props.type === 'o2') ?
 		<Paragraph role="paragraph" {...props}>
 			{props.children}
 		</Paragraph>
-	)
+	: (props.type === 'b1' || props.type === 'b2' || props.type === 'b3') ?
+		<Button role="button" {...props}>
+			{props.children}
+		</Button>
+	: null)
 }
 export default Text;
-export interface Props extends ComponentPropsWithoutRef<"p">{
-    type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'p2' | 'c1' | 'c2'
-    weight?: 'semibold' | 'medium' | 'regular'
-}
