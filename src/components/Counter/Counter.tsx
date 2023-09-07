39f0ae5a83e5420f16b9cda0e2b74dabb1dddeaa
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2023-09-07 17:06:10 +0200

    feat: counter animation

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,50 +1,101 @@
-import styled from "styled-components";
+import { useState, useEffect, useRef, useMemo, Fragment } from 'react';
+import styles from './styles.module.css';
 
-import Text from "../Text/Text";
-import { ColorV2 } from "../../constants";
+import FlipClockDigit from './CounterDigit';
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: fit-content;
-    gap: 2px;
-`
-const Cell = styled.div`
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 40px;
-    width: 28px;
-    border-radius: 6px;
-    box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.04);
-    background-color: white;
-`
-const Line = styled.div`
-    position: absolute;
-    top: 20px;
-    width: 100%;
-    border-bottom: 1px solid ${ColorV2.surface.background};
-`
+function Counter(props: Props) {
 
-const Counter = (props: Props) =>{
+    const [ counter, setCounter ] = useState(props.amount > 0 ? props.amount-10 : 0);
+    const counterRef = useRef(counter);
+    counterRef.current = counter;
+    const countdownRef = useRef(0);
 
-    const chars = props.amount.toString().split("");
+    useEffect(() => {
+        clearTimer();
+        setCounter(props.amount > 0 ? props.amount-10 : 0)
+        countdownRef.current = window.setInterval(tick, 400);
+        return () => clearTimer();
+    }, [props.amount]);
 
-    return(
-        <Container>
-            {chars.map((item) =>(
-                <Cell>
-                    <Line/>
-                    <Text type='h3' weight='semibold' style={{color: ColorV2.text.primary, zIndex: 1}}>
-                        {item}
-                    </Text>
-                </Cell>
-            ))}
-        </Container>
-    )
+    function clearTimer() {
+        window.clearInterval(countdownRef.current);
+    }
+
+    function tick() {
+        setCounter(counterRef.current+1);
+        if(counterRef.current+1 >= props.amount) {
+            clearTimer();
+        }
+    }
+
+    function parseAmount(amount: number) { 
+        const length = props.amount.toString().length;
+        const currentSplit = amount.toString().split("");
+        const nextSplit = (amount+1).toString().split("");
+        const auxSplit = [];
+
+        for(var i=0; i<length; i++){
+            if((length - currentSplit.length)-i === 0){
+                break;
+            }
+            else{
+                auxSplit.push("0");
+            }
+        }
+        return {
+            current: currentSplit,
+            next: nextSplit
+        };
+    }
+
+    const sections = useMemo(() => {
+        const formatted = parseAmount(counter);
+        return [ formatted ];
+    }, [counter]);
+
+    const containerStyle = useMemo(() => {
+        const t: any = {
+            '--fcc-digit-color': props.color,
+            '--fcc-background': props.backgroundColor,
+            '--fcc-digit-block-width': props.width && props.width+'px',
+            '--fcc-digit-block-height': props.height && props.height+'px',
+            '--fcc-digit-font-size': props.fontSize && props.fontSize+'px'
+        }
+        return t
+    }, [props]);
+
+    return (
+        <div
+            className={styles.fcc__container}
+            data-testid='fcc-container'
+            style={containerStyle}
+        >
+            {sections.map((item, idx) => {
+                //console.log(item);
+                return (
+                    <Fragment key={`digit-block-${idx}`}>
+                        <div className={styles.fcc__digit_block_container}>
+                            {item.current.map((cItem, cIdx) => (
+                                <FlipClockDigit 
+                                    key={cIdx} 
+                                    current={item.current[cIdx]} 
+                                    next={item.next[cIdx]}
+                                />
+                            ))}
+                        </div>
+                    </Fragment>
+                );
+            })}
+        </div>
+    );
 }
+
 export default Counter;
 export interface Props{
-    amount: number
+    height: number,
+    width: number,
+    amount: number,
+    color?: string,
+    backgroundColor?: string,
+    fontSize?: number
 }
