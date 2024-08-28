7afdeda5c10cd8f79380c1b3e4876b7295b93b90
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2024-08-28 09:26:24 +0200

    chore: storybook update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,16 +2,25 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions"
   ],
+
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
+
   staticDirs: ["../public"],
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
