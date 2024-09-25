abf4cac4c54d8ce7f175641a570882e26af4a5dc
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2024-09-25 16:05:08 +0200

    fix: text area export

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,7 +13,7 @@ export { default as Avatar } from "./Avatar/Avatar";
 export { default as Filter } from "./Filter/Filter";
 export { default as Checkbox } from "./Checkbox/CheckboxList";
 export { default as Dropdown } from "./Dropdown/Dropdown";
-export { default as TextArea } from "./TextArea/TextAreaEditor";
+export { default as TextArea } from "./TextArea/TextArea";
 export { default as RadioButton } from "./RadioButton/RadioButtonList";
 export { default as CellSelector } from "./CellSelector/CellSelector";
 export { default as Tabs } from "./Tabs/Tabs";
