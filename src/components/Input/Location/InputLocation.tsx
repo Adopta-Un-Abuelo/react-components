e5897a4cabcb640977947262959af1be91eec9a6
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2025-07-03 09:56:15 +0200

    fix: input location google maps api modification

--- src/components/Input/Location/InputLocation.tsx
+++ src/components/Input/Location/InputLocation.tsx
@@ -1,6 +1,6 @@
-import { APIProvider, useMapsLibrary } from "@vis.gl/react-google-maps";
-import { FormEvent, useCallback, useEffect, useState } from "react";
-import { useAutocompleteSuggestions } from "./use-autocomplete-suggestions";
+import React, { useState, useRef, useCallback, useEffect } from "react";
+import { LoadScript } from "@react-google-maps/api";
+import debounce from "lodash.debounce";
 import Input, { InputProps } from "../Basic/Input";
 import styled from "styled-components";
 import { ColorV2 } from "../../../constants";
@@ -39,235 +39,217 @@ const SuggestionText = styled(Text)`
 	border-radius: 8px;
 `;
 
+const GOOGLE_MAP_LIBRARIES: any = ["places"];
+
 const InputLocation = ({
 	googleAPIKey,
 	isForm,
 	onLocationChange,
-	error,
 	...restProps
 }: InputLocationProps) => {
+	const autocompleteService =
+		useRef<google.maps.places.AutocompleteService | null>(null);
+	const placesService = useRef<google.maps.places.PlacesService | null>(null);
+
+	const [input, setInput] = useState("");
+	const [predictions, setPredictions] = useState<
+		google.maps.places.AutocompletePrediction[]
+	>([]);
 	const [errorString, setErrorString] = useState<string | undefined>(
 		undefined
 	);
-	const [inputValue, setInputValue] = useState<string | undefined>(undefined);
 
-	useEffect(() => {
-		setErrorString(error);
-	}, [error]);
+	const handleScriptLoad = () => {
+		if (typeof google === "undefined") return;
 
-	useEffect(() => {
-		if (restProps.defaultValue)
-			setInputValue(restProps.defaultValue as string);
-		else setInputValue("");
-	}, [restProps.defaultValue]);
+		const mapDiv = document.createElement("div");
+		const dummyMap = new google.maps.Map(mapDiv);
 
-	const onPlaceSelect = async (place: google.maps.places.Place) => {
-		setErrorString(undefined);
-		if (place.addressComponents && place.location) {
-			const routeObj = place.addressComponents.filter((it) =>
-				it.types.includes("route")
-			);
-			const route =
-				routeObj.length > 0 ? routeObj[0].longText : undefined;
-
-			const zipCodeObj = place.addressComponents.filter((it) =>
-				it.types.includes("postal_code")
-			);
-			const zipCode =
-				zipCodeObj.length > 0 ? zipCodeObj[0].longText : undefined;
-
-			if (route && zipCode) {
-				const routeNumberObj = place.addressComponents.filter((it) =>
-					it.types.includes("street_number")
-				);
-				const routeNumber =
-					routeNumberObj.length > 0
-						? routeNumberObj[0].longText
-						: undefined;
-
-				const cityObj = place.addressComponents.filter((it) =>
-					it.types.includes("locality")
-				);
-				const city =
-					cityObj.length > 0 ? cityObj[0].longText : undefined;
-
-				const provinceObj = place.addressComponents.filter((it) =>
-					it.types.includes("administrative_area_level_2")
-				);
-				const province =
-					provinceObj.length > 0
-						? provinceObj[0].longText
-						: undefined;
-
-				const countryObj = place.addressComponents.filter((it) =>
-					it.types.includes("country")
-				);
-				const country =
-					countryObj.length > 0 ? countryObj[0].shortText : undefined;
-
-				const addressString = `${route}${
-					routeNumber ? " " + routeNumber : ""
-				}, ${zipCode ? zipCode : ""}, ${city}, ${province}, ${country}`;
-				const sortAddress = `${city ? city + ", " : ""}${
-					province ? province + ", " : ""
-				}${country}`;
-				const tempLocation = {
-					address: addressString,
-					sortAddress: sortAddress,
-					route: route as string,
-					routeNumber: routeNumber as string,
-					location: {
-						lat: place.location.lat(),
-						lng: place.location.lng(),
-					},
-					country: country as string,
-					city: city as string,
-					province: province as string,
-					zipCode: zipCode as string,
-				};
-
-				//Get the timezone. Skip if it is a form and there is no route and route number
-				if (!isForm || (route && routeNumber)) {
-					const response = await fetch(
-						`https://maps.googleapis.com/maps/api/timezone/json?location=${tempLocation.location.lat},${tempLocation.location.lng}&timestamp=1331161200&key=${googleAPIKey}`
-					);
-					const result2 = await response.json();
-					if (result2.status === "OK") {
-						onLocationChange &&
-							onLocationChange({
-								...tempLocation,
-								timeZone: result2.timeZoneId,
-							});
-					} else {
-						onLocationChange && onLocationChange(tempLocation);
-					}
-				} else {
-					onLocationChange && onLocationChange(tempLocation);
-				}
-				if (isForm) {
-					setInputValue(
-						`${route}${routeNumber ? " " + routeNumber : ""}`
-					);
-				} else {
-					setInputValue(addressString);
-				}
-			} else {
-				setErrorString(
-					"Necesitamos una dirección más precisa para encontrar tu código postal. Por favor, indica el número de la calle."
-				);
-			}
-		} else {
-			setErrorString(
-				"Parece que hubo un error de conexión al obtener la dirección. Prueba de nuevo más tarde."
-			);
-		}
+		autocompleteService.current =
+			new google.maps.places.AutocompleteService();
+		placesService.current = new google.maps.places.PlacesService(dummyMap);
 	};
 
-	return (
-		<APIProvider apiKey={googleAPIKey}>
-			<PlaceAutocomplete
-				onPlaceSelect={onPlaceSelect}
-				error={errorString}
-				value={inputValue}
-				{...restProps}
-			/>
-		</APIProvider>
-	);
-};
-
-const PlaceAutocomplete = ({ onPlaceSelect, value, ...restProps }: Props) => {
-	const places = useMapsLibrary("places");
-
-	const [searchValue, setSearchValue] = useState<string>("");
-	const [inputValue, setInputValue] = useState<
-		string | number | readonly string[] | undefined
-	>("");
-	const [searchTimeout, setSearchTimeout] = useState<
-		NodeJS.Timeout | undefined
-	>(undefined);
-	const { suggestions, resetSession } = useAutocompleteSuggestions(
-		searchValue,
-		{
-			language: "es",
-			includedPrimaryTypes: ["route", "street_address"],
-		}
+	const fetchPredictions = useCallback(
+		debounce((value: string) => {
+			if (!autocompleteService.current || !value) return;
+			autocompleteService.current.getPlacePredictions(
+				{
+					input: value,
+					types: ["address"],
+				},
+				(results) => {
+					setPredictions(results || []);
+				}
+			);
+		}, 500),
+		[]
 	);
 
-	useEffect(() => {
-		if (value) setInputValue(value);
-		else setInputValue("");
-	}, [value]);
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value;
+		setInput(value);
+		fetchPredictions(value);
+	};
 
-	const handleInput = useCallback(
-		(event: FormEvent<HTMLInputElement>) => {
-			const value = (event.target as HTMLInputElement).value;
-			setInputValue(value);
-			if (searchTimeout) {
-				clearTimeout(searchTimeout);
-			}
-			const timeout = setTimeout(() => {
-				setSearchValue(value);
-			}, 500);
-			setSearchTimeout(timeout);
-		},
-		[searchTimeout]
-	);
+	const handleSelect = (placeId: string) => {
+		if (!placesService.current) return;
 
-	const handleSuggestionClick = useCallback(
-		async (suggestion: google.maps.places.AutocompleteSuggestion) => {
-			if (!places) return;
-			if (!suggestion.placePrediction) return;
+		placesService.current.getDetails(
+			{
+				placeId,
+				fields: ["geometry", "address_components"],
+			},
+			(place) => {
+				if (
+					place &&
+					place.geometry &&
+					place.geometry.location &&
+					place.address_components
+				) {
+					setPredictions([]);
+					setErrorString(undefined);
 
-			const place = suggestion.placePrediction.toPlace();
-			await place.fetchFields({
-				fields: ["location", "addressComponents"],
-			});
+					const routeObj = place.address_components.filter((it) =>
+						it.types.includes("route")
+					);
+					const route =
+						routeObj.length > 0 ? routeObj[0].long_name : undefined;
 
-			setSearchValue("");
-			resetSession();
-			onPlaceSelect(place);
-		},
-		[places, onPlaceSelect]
-	);
+					const zipCodeObj = place.address_components.filter((it) =>
+						it.types.includes("postal_code")
+					);
+					const zipCode =
+						zipCodeObj.length > 0
+							? zipCodeObj[0].long_name
+							: undefined;
 
-	return (
-		<SearchView className="autocomplete-container">
-			<Input
-				value={inputValue}
-				onInput={(event) => handleInput(event)}
-				containerStyle={{
-					display: "flex",
-					flex: 1,
-					flexDirection: "column",
-				}}
-				{...restProps}
-			/>
-			{suggestions.length > 0 && (
-				<DropdownMenu className="custom-list">
-					{suggestions.map((suggestion, index) => {
-						return (
-							<SuggestionView
-								key={index}
-								className="custom-list-item"
-								onClick={() =>
-									handleSuggestionClick(suggestion)
-								}
-							>
-								<SuggestionText type="p2">
-									{suggestion.placePrediction?.text.text}
-								</SuggestionText>
-							</SuggestionView>
+					if (route && zipCode) {
+						const routeNumberObj = place.address_components.filter(
+							(it) => it.types.includes("street_number")
 						);
-					})}
-				</DropdownMenu>
+						const routeNumber =
+							routeNumberObj.length > 0
+								? routeNumberObj[0].long_name
+								: undefined;
+
+						const cityObj = place.address_components.filter((it) =>
+							it.types.includes("locality")
+						);
+						const city =
+							cityObj.length > 0
+								? cityObj[0].long_name
+								: undefined;
+
+						const provinceObj = place.address_components.filter(
+							(it) =>
+								it.types.includes("administrative_area_level_2")
+						);
+						const province =
+							provinceObj.length > 0
+								? provinceObj[0].long_name
+								: undefined;
+
+						const countryObj = place.address_components.filter(
+							(it) => it.types.includes("country")
+						);
+						const country =
+							countryObj.length > 0
+								? countryObj[0].short_name
+								: undefined;
+
+						const addressString = `${route}${
+							routeNumber ? " " + routeNumber : ""
+						}, ${
+							zipCode ? zipCode : ""
+						}, ${city}, ${province}, ${country}`;
+						const sortAddress = `${city ? city + ", " : ""}${
+							province ? province + ", " : ""
+						}${country}`;
+						const tempLocation = {
+							address: addressString,
+							sortAddress: sortAddress,
+							route: route as string,
+							routeNumber: routeNumber as string,
+							location: {
+								lat: place.geometry.location.lat(),
+								lng: place.geometry.location.lng(),
+							},
+							country: country as string,
+							city: city as string,
+							province: province as string,
+							zipCode: zipCode as string,
+						};
+						onLocationChange && onLocationChange(tempLocation);
+						if (isForm) {
+							setInput(
+								`${route}${
+									routeNumber ? " " + routeNumber : ""
+								}`
+							);
+						} else {
+							setInput(addressString);
+						}
+					} else {
+						setErrorString(
+							"Necesitamos una dirección más precisa para encontrar tu código postal. Por favor, indica el número de la calle."
+						);
+					}
+				} else {
+					setErrorString(
+						"Parece que hubo un error de conexión al obtener la dirección. Prueba de nuevo más tarde."
+					);
+				}
+			}
+		);
+	};
+	return (
+		<LoadScript
+			googleMapsApiKey={googleAPIKey}
+			libraries={GOOGLE_MAP_LIBRARIES}
+			onLoad={handleScriptLoad}
+		>
+			<SearchView className="autocomplete-container">
+				<Input
+					value={input}
+					onChange={handleInputChange}
+					containerStyle={{
+						display: "flex",
+						flex: 1,
+						flexDirection: "column",
+					}}
+					{...restProps}
+				/>
+				{predictions.length > 0 && (
+					<DropdownMenu className="custom-list">
+						{predictions.map((suggestion, index) => {
+							return (
+								<SuggestionView
+									key={index}
+									className="custom-list-item"
+									onClick={() =>
+										handleSelect(suggestion.place_id)
+									}
+								>
+									<SuggestionText type="p2">
+										{suggestion.description}
+									</SuggestionText>
+								</SuggestionView>
+							);
+						})}
+					</DropdownMenu>
+				)}
+			</SearchView>
+			{errorString && (
+				<Text
+					type="c1"
+					style={{ color: ColorV2.text.red, padding: "0px 4px" }}
+				>
+					{errorString}
+				</Text>
 			)}
-		</SearchView>
+		</LoadScript>
 	);
-};
-type Props = {
-	error?: string;
-	value?: string | number | readonly string[] | undefined;
-	onPlaceSelect: (place: google.maps.places.Place) => void;
 };
 export default InputLocation;
 export type InputLocationProps = InputProps & {
