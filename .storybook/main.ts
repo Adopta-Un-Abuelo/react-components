cd980c2d2e5037a1f4e80ede64b25abf0c32349d
Guillermo Angeles <guillermo.angeles@adoptaunabuelo.com>
2025-12-09 13:25:12 +0100

    fix: package update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,8 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@chromatic-com/storybook",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y"
   ],
 
   framework: {
